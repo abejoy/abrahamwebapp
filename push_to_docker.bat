87302346df010eae8e69bc75095ffcbf88fa9a12
abrah <abrahamjoys98@gmail.com>
2020-06-02 21:35:12 +0100

    comming soon message updated again

--- push_to_docker.bat
+++ push_to_docker.bat
@@ -1,5 +1,5 @@
 call mvn clean install
 call cd abrahamreactwebsite
 call docker build . -t abrahamjoys98/abrahamwebsite
-call docker tag abrahamjoys98/abrahamwebsite:latest abrahamjoys98/abrahamwebsite:0.0.1
-call docker push abrahamjoys98/abrahamwebsite:0.0.1
+call docker tag abrahamjoys98/abrahamwebsite:latest abrahamjoys98/abrahamwebsite:0.0.2
+call docker push abrahamjoys98/abrahamwebsite:0.0.2
