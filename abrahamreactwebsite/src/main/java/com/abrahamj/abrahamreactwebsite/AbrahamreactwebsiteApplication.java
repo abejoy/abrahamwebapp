535859843fa83b92dbf5e9c35f0bc57338eb54dc
abrah <abrahamjoys98@gmail.com>
2020-06-02 20:07:52 +0100

    first commit

---
+++ abrahamreactwebsite/src/main/java/com/abrahamj/abrahamreactwebsite/AbrahamreactwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.abrahamj.abrahamreactwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbrahamreactwebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbrahamreactwebsiteApplication.class, args);
+    }
+
+}
---
+++ abrahamreactwebsite/src/test/java/com/abrahamj/abrahamreactwebsite/AbrahamreactwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrahamj.abrahamreactwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbrahamreactwebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
